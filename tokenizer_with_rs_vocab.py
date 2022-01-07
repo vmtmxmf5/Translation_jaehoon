ed21bee0d67a6da97673919ec2d7614d85261479
vmtmxmf5 <72297960+vmtmxmf5@users.noreply.github.com>
2022-01-07 18:18:52 +0900

    Update tokenizer_with_rs_vocab.py

--- tokenizer_with_rs_vocab.py
+++ tokenizer_with_rs_vocab.py
@@ -3,17 +3,27 @@ import json
 
 if __name__=='__main__':
     # 여기서 생성되는 vocab은 단순히 ref. 용이다
-    # spm.SentencePieceTrainer.train(input=['wmt16_src_train.txt', 'wmt16_tgt_train.txt'],
-    #                                 model_prefix='wmt16',
+    # spm.SentencePieceTrainer.train(input='wmt16_src_train.txt',
+    #                                 model_prefix='wmt16_src',
     #                                 vocab_size=8000,
     #                                 unk_id=0,
     #                                 pad_id=1,
     #                                 bos_id=2,
     #                                 eos_id=3,
-    #                                 input_sentence_size=6000000
     #                                 )
     sp = spm.SentencePieceProcessor()
-    sp.load('wmt16.model')
+    sp.load('wmt16_src.model')
+
+    # spm.SentencePieceTrainer.train(input='wmt16_tgt_train.txt',
+    #                             model_prefix='wmt16_src',
+    #                             vocab_size=8000,
+    #                             unk_id=0,
+    #                             pad_id=1,
+    #                             bos_id=2,
+    #                             eos_id=3,
+    #                             )
+    # sp = spm.SentencePieceProcessor()
+    # sp.load('wmt16_src.model')
 
     print(sp.encode_as_pieces('this is a test.'))
 
@@ -22,27 +32,27 @@ if __name__=='__main__':
 
     # Aggregates the frequency of each token in the training data.
     freq = {}
-    with open('wmt16_src_train.txt', 'r') as f:
+    with open('wmt16_src_train.txt', 'rb') as f:
         for line in f:
-            line = line.rstrip()
+            line = line.encode('utf-8').rstrip()
             for piece in sp.encode_as_pieces(line):
                 freq.setdefault(piece, 0)
                 freq[piece] += 1
                 
     vocabs = list(filter(lambda x : x in freq and freq[x] > 1000, vocabs))
 
-    freq = {}
-    with open('wmt16_tgt_train.txt', 'r') as f:
-        for line in f:
-            line = line.rstrip()
-            for piece in sp.encode_as_pieces(line):
-                freq.setdefault(piece, 0)
-                freq[piece] += 1
+    # freq = {}
+    # with open('wmt16_tgt_train.txt', 'r') as f:
+    #     for line in f:
+    #         line = line.rstrip()
+    #         for piece in sp.encode_as_pieces(line):
+    #             freq.setdefault(piece, 0)
+    #             freq[piece] += 1
                 
-    vocabs = list(filter(lambda x : x in freq and freq[x] > 1000, vocabs))
+    # vocabs = list(filter(lambda x : x in freq and freq[x] > 1000, vocabs))
     
     # Save the new vocab
-    with open('restriced_vocab', 'w') as f:
+    with open('restriced_vocab_src.json', 'w', encoding='utf-8') as f:
         json.dump(vocabs, f)
 
     sp.set_vocabulary(vocabs)
